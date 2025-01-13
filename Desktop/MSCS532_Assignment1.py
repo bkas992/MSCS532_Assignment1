8a84de60c8a1ca05604937b43737a7097b2d0dae
Bikash Shrestha <bshrestha32645@ucumberlands.edu>
2025-01-12 18:04:04 -0600

    Adding comments

--- Desktop/MSCS532_Assignment1.py
+++ Desktop/MSCS532_Assignment1.py
@@ -5,7 +5,7 @@ example_arr = [7, 3, 10, 22, 21, 9, 12, 5]
 def insertion_sort_dec(example_arr):
     
     for i in range(1, len(example_arr)):
-        current_element = example_arr[i]
+        current_element = example_arr[i] #Assigning the element to be insorted in sorted array
         j = i-1 
 
         #Performing the check of each elements in array of the array
@@ -17,5 +17,6 @@ def insertion_sort_dec(example_arr):
 
     return example_arr
 
+#Giving name to the sorted array and then printing
 arr_sorted_dec = insertion_sort_dec(example_arr)
 print("Array sorted in decreasing order:", arr_sorted_dec)
